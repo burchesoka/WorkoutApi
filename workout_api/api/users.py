e9de99750d329eea9140b94e699746199176d082
burchesoka <dvkotin@yandex.ru>
2022-06-27 10:32:22 +0500

    Добавил методы в апи

--- workout_api/api/users.py
+++ workout_api/api/users.py
@@ -25,12 +25,27 @@ def get_users(
     user_status: Optional[models.UserStatus] = None,
     users_service: UsersService = Depends(),
 ):
-
     return users_service.get_users(user_status=user_status)
 
 
+@router.get('/{user_id}', response_model=models.User,)
+def get_user(
+    user_id: int,
+    users_service: UsersService = Depends(),
+):
+    return users_service.get(user_id)
+
+
+@router.get('/telegram/{telegram_id}', response_model=models.User)
+def get_user_by_telegram_id(
+        telegram_id: int,
+        user_service: UsersService = Depends()
+):
+    return user_service.get_user_by_telegram_id(telegram_id)
+
+
 @router.post(
-    '/create',
+    '/',
     response_model=models.User,
     status_code=status.HTTP_201_CREATED,
 )
@@ -39,4 +54,23 @@ def create_user(
     users_service: UsersService = Depends(),
 
 ):
-    return users_service.create_user(user_data)
+    return users_service.create(user_data)
+
+
+@router.put('/{user_id}', response_model=models.User)
+def update_user(
+        user_id: int,
+        user_data: models.UserUpdate,
+        users_service: UsersService = Depends()
+):
+    return users_service.update(user_id, user_data)
+
+
+@router.delete('/{user_id}')
+def delete_user(
+    user_id: int,
+    users_service: UsersService = Depends(),
+
+):
+    users_service.delete(user_id)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- workout_api/services/users.py
+++ workout_api/services/users.py
@@ -20,7 +20,32 @@ class UsersService:
         users = query.all()
         return users
 
-    def create_user(self, user_data: models.UserCreate) -> tables.User:
+    def _get(self, user_id):
+        user = (
+            self.session
+            .query(tables.User)
+            .filter_by(id=user_id)
+            .first()
+        )
+        if not user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        return user
+
+    def get(self, user_id: int) -> tables.User:
+        return self._get(user_id)
+
+    def get_user_by_telegram_id(self, telegram_id: int) -> tables.User:
+        user = (
+            self.session
+            .query(tables.User)
+            .filter_by(telegram_id=telegram_id)
+            .first()
+        )
+        if not user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        return user
+
+    def create(self, user_data: models.UserCreate) -> tables.User:
         user = tables.User(**user_data.dict())
         self.session.add(user)
         try:
@@ -28,3 +53,15 @@ class UsersService:
         except IntegrityError as e:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f'{e}')
         return user
+
+    def update(self, user_id: int, user_data: models.UserUpdate) -> tables.User:
+        user = self._get(user_id)
+        for field, value in user_data:
+            setattr(user, field, value)
+        self.session.commit()
+        return user
+
+    def delete(self, user_id):
+        user = self._get(user_id)
+        self.session.delete(user)
+        self.session.commit()
